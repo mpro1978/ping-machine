531cde54e58adaeea681927a2e1f70456c7040a2
mpro1978 <anmeldung_mp@hood-diver.com>
2018-10-19 04:39:03 +0200

    ... der Ausgangscode

---
+++ Bursche2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# check and log if a host is reachable by ping
+
+#CONFIGURATION
+
+#IP of host
+WATCH_IP="192.168.178.66"
+#path to logfile
+LOGFILE="/home/markus/test/watchip.log"
+#duration between pings
+PAUSE=3
+#how many failed pings before log
+TESTS=2
+
+#SCRIPT
+
+#initialize
+MISSED=0
+touch $LOGFILE
+
+while true; do
+  if ! ping -c 1 -w 1 $WATCH_IP > /dev/null; then
+    ((MISSED++))
+  else
+    if [ $MISSED -ge $TESTS ]; then
+      echo `date` '-' $WATCH_IP "is up again." >> $LOGFILE;
+    fi
+    MISSED=0
+  fi;
+  if [ $MISSED -eq $TESTS ]; then
+    echo `date` "-" $WATCH_IP "is down." >> $LOGFILE;
+  fi
+  sleep $PAUSE;
+done
